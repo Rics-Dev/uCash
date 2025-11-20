cf3b25af44edbd9f8329f539cf128cf4b9b689d1
Ric's Dev <ricdev.io@gmail.com>
2025-11-20 21:12:40 +0100

    should be fixed

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,7 +1,10 @@
 import type { auth } from "@uCash/auth";
 import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
+import { polarClient } from "@polar-sh/better-auth";
+
 
 export const authClient = createAuthClient({
-	plugins: [inferAdditionalFields<typeof auth>()],
+	plugins: [inferAdditionalFields<typeof auth>(), polarClient()],
 });
+
